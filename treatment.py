e6ee41ccb912249c1bd90e676f0648e3743da04a
João Gabriel <116111509+joaoluna-dev@users.noreply.github.com>
2025-04-12 19:50:53 -0300

    Update treatment.py
    Improvement of the script, which can now perform multiple treatments for various molecules

--- treatment.py
+++ treatment.py
@@ -1,10 +1,29 @@
 import sys
-import subprocess
-import os
-from main import log_write
+from main import log_write, json_write
+
+def remove_molecules(inpt, outpt, mol):
+    mol_set = set(mol)
+    with open(inpt, "r") as infile, open(outpt, "w") as outfile:
+        for line in infile:
+            keep_line = True
+            splitting = line.split(" ")
+            for element in splitting:
+                if element not in mol_set:
+                    continue
+                else:
+                    keep_line = False
+            if keep_line:
+                outfile.write(line)
+
+#----------------------------------------------------------------------------------------------#
+
+def get_molecules_list(res_names):
+    return res_names.split(",")
+
+#-----------------------------------------------------------------------------------------------#
 
 if len(sys.argv) < 3:
-    print("Erro: Argumentos insuficientes.")
+    print("Error: Insufficient arguments.")
     sys.exit(1)
 
 workspace = sys.argv[1]
@@ -12,39 +31,20 @@ protein_code = sys.argv[2]
 input_file = sys.argv[3]
 output_file = sys.argv[4]
 
-with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-    for line in infile:
-        if "HOH" not in line:
-            outfile.write(line)
-log_write("Arquivo tratado para a remoção de resíduos de água.", "info")
-print(f"Arquivo tratado para remoção de água salvo como {protein_code}_tratado.pdb.")
+residues_removed = False
 
 while True:
-    try:
-        selection_HETATM = input("Você deseja remover algo a mais? (y/n): ").lower()    
-        if selection_HETATM == "y":
-            resn = input("Insira o nome do resíduo a ser removido: ")
-            input_file_2 = output_file
-            output_file_2 = os.path.join(workspace, f"{protein_code}_tratado_{resn}.pdb")
-            with open(output_file, "r") as infile, open(output_file_2, "w") as outfile:
-                for line in infile:
-                    if f"{resn}" not in line:
-                        outfile.write(line)
-                with open('logs.txt', 'a') as log:
-                    log_write("Usuário solicitou remoção de resíduo", "info")
-                    log_write(f"Arquivo tratado para a remoção de {resn} e salvo como {output_file_2}.", "info")
-                print(f"Arquivo tratado salvo como {protein_code}_tratado_{resn}.pdb.")
-            break
-        elif selection_HETATM == "n":
-            break
-        else:
-            print("Entrada inválida. Tente novamente.")
-            log_write(f"Entrada inserida inválida para a seleção de heteroátomo", "erro")
-    except ValueError:
-        log_write("Entrada inválida inserida.", "erro")
-        print("Entrada inválida. Tente novamente.")
-    except Exception as e:
-        print(f"Ocorreu um erro inesperado: {e}")
-        log_write(f"{e}", "erro")
-        print("Erro adicionado ao log do sistema.")
+    selection_residue = input("Do you want to remove anything from the file? (y/n): ")
+    if selection_residue == "y":
+        residues_name = input("Insert molecule or molecules name, separated by a comma: ")
+        molecule = get_molecules_list(residues_name)
+        remove_molecules(input_file, output_file, molecule)
+        residues_removed = True
+        log_write("Residues removed", "info")
+        print(f"Treated file saved as {output_file}.")
+        log_write(f"Treated file saved as {output_file}", "info")
+    elif selection_residue == "n":
+        break
 
+json_write({"Molecules were removed:": residues_removed})
+json_write({"Molecules removed": molecule})
