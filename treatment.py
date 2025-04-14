2a9a8eef40626b5857ec165169bed180bef7368f
João Gabriel <116111509+joaoluna-dev@users.noreply.github.com>
2025-04-14 11:25:39 -0300

    Update treatment.py

--- treatment.py
+++ treatment.py
@@ -1,5 +1,7 @@
 import sys
-from main import log_write, json_write
+import os
+
+#-----------------------------------------Functions-------------------------------------------#
 
 def remove_molecules(inpt, outpt, mol):
     mol_set = set(mol)
@@ -15,23 +17,24 @@ def remove_molecules(inpt, outpt, mol):
             if keep_line:
                 outfile.write(line)
 
-#----------------------------------------------------------------------------------------------#
 
 def get_molecules_list(res_names):
     return res_names.split(",")
 
-#-----------------------------------------------------------------------------------------------#
+#--------------------------------------------Values--------------------------------------------#
 
 if len(sys.argv) < 3:
-    print("Error: Insufficient arguments.")
+    print("Error: Insufficient arguments. Usage: python treatment.py pdb_ID filename")
     sys.exit(1)
 
-workspace = sys.argv[1]
+
 protein_code = sys.argv[2]
 input_file = sys.argv[3]
-output_file = sys.argv[4]
-
+workspace = os.path.dirname(os.path.abspath(__file__))
+output_file = f"{protein_code}_treated.pdb"
 residues_removed = False
+
+#---------------------------------------Main Script--------------------------------------------#
 
 while True:
     selection_residue = input("Do you want to remove anything from the file? (y/n): ")
@@ -40,11 +43,8 @@ while True:
         molecule = get_molecules_list(residues_name)
         remove_molecules(input_file, output_file, molecule)
         residues_removed = True
-        log_write("Residues removed", "info")
         print(f"Treated file saved as {output_file}.")
-        log_write(f"Treated file saved as {output_file}", "info")
     elif selection_residue == "n":
         break
 
-json_write({"Molecules were removed:": residues_removed})
-json_write({"Molecules removed": molecule})
+
